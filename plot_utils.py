a928cc278b3f602a9d7196254b0bdc0c1c82fb2e
Brianna Abam <brianna@takarasystems.com>
2025-11-25 14:22:39 -0500

    Created framework testing and plotting standalon python modules

---
+++ plot_utils.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import os
+
+def plot_acc(accuracies, test_type, labels, n_clients, title):
+    os.makedirs(f'plots/{test_type}', exist_ok=True)  # creates 'plots/{test_type}' if it doesn't exist to separate depending on what test we are using
+    # Plot the accuracies
+    plt.figure(figsize=(10, 6))
+
+    for framework_idx, framework_acc in enumerate(accuracies):
+        plt.plot(framework_acc, label = f"{labels[framework_idx]}")
+    
+    # Add labels and title
+    plt.xlabel('Communication Rounds')
+    plt.ylabel('Accuracy')
+    plt.title(f"{title} ({n_clients} clients total)")
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(f'plots/{test_type}/test_accuracy_{test_type}_{n_clients}clnts.png')
+
+def plot_loss(losses, test_type, labels, n_clients, title):
+    os.makedirs(f'plots/{test_type}', exist_ok=True)  # creates 'plots/{test_type}' if it doesn't exist to separate depending on what test we are using
+    # Plot the losses
+    plt.figure(figsize=(10, 6))
+    
+    for framework_idx, framework_loss in enumerate(losses):
+        plt.plot(framework_loss, label = f"{labels[framework_idx]}")
+    
+    # Add labels and title
+    plt.xlabel('Communication Round Time (in sec)')
+    plt.ylabel('Loss')
+    plt.title(f"{title} ({n_clients} clients total)")
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(f'plots/{test_type}/test_loss_{test_type}_{n_clients}clnts.png')
+
+
+def plot_comm_times(comm_times, test_type, labels, n_clients, title):
+    os.makedirs(f'plots/{test_type}', exist_ok=True)  # creates 'plots/{test_type}' if it doesn't exist to separate depending on what test we are using
+    # Plot the losses
+    plt.figure(figsize=(10, 6))
+    
+    for framework_idx, framework_comm_times in enumerate(comm_times):
+        plt.plot(framework_comm_times, label = f"{labels[framework_idx]} / mean time = {np.mean(framework_comm_times):0.2f}")
+    
+    # Add labels and title
+    plt.xlabel('Communication Rounds')
+    plt.ylabel('Communication Times')
+    plt.title(f"{title} ({n_clients} clients total)")
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(f'plots/{test_type}/test_comm_times_{test_type}_{n_clients}clnts.png')
+
---
+++ test_framework_utils.py
@@ -0,0 +1,139 @@
+from models import ConvNet
+from data_utils import build_mnist
+from train_utils import fl_semidecentralized_cluster_train, fl_centralized_train, fl_fullydecentralized_cluster_train
+from plot_utils import plot_acc, plot_loss, plot_comm_times
+
+def test_cluster_framework_variations(test_type, args):
+    
+    if test_type == "T1_eval_against_baselines":
+
+        # Test Description:
+        # In this test, we will be evaluating our framework's performance against the baselines (being decentralized server architectire and
+        # a central server gossip network architecture). We evaluate the accuracy, loss, and communication round time, and plot the 
+        # results.
+
+        n_clients = 50
+    
+        # get all of the client data and the testloader
+        client_data, testloader = build_mnist(n_clients, args.iid_alpha, args.batch_size, seed=args.seed)
+
+        # ------------------------
+        # creating the parameter server model for the fully decentralized arch
+        decentralized_arch_server_model = ConvNet()
+        # creating the parameter server model for the decentralized arch
+        accuracies_decentralized_comm, losses_decentralized_comm, times_decentralized_comm = fl_fullydecentralized_cluster_train(decentralized_arch_server_model, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader)
+        # ------------------------
+
+        # ------------------------
+        # creating the parameter server model for the cluster arch
+        cluster_arch_server_model = ConvNet()
+        # our framework
+        accuracies_cluster_comm, losses_cluster_comm, times_cluster_comm = fl_semidecentralized_cluster_train(cluster_arch_server_model, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader, "T1_eval_against_baselines", num_clusters=5)
+        # ------------------------
+
+        # ------------------------
+        # creating the parameter server model for the central arch
+        central_arch_server_model = ConvNet()
+        # centralized fully sync SGD
+        accuracies_central_comm, losses_central_comm, times_central_comm = fl_centralized_train(central_arch_server_model, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader)
+        # ------------------------
+
+        plot_labels = ["Fully Decentralized Framework", "Our Semi-decentralized Framework", "Fully Sync SGD (centralized)"]
+        accuracies = [accuracies_decentralized_comm, accuracies_cluster_comm, accuracies_central_comm]
+        losses = [losses_decentralized_comm, losses_cluster_comm, losses_central_comm]
+        times = [times_decentralized_comm, times_cluster_comm, times_central_comm]
+        
+        # plot everything
+        plot_acc(accuracies, test_type, plot_labels, n_clients, 'Accuracy w/ eq num of clients per cluster')
+        plot_loss(losses, test_type, plot_labels, n_clients, 'Loss w/ eq num of clients per cluster')
+        plot_comm_times(times, test_type, plot_labels, n_clients, 'Communication Times w/ eq num of clients per cluster')
+
+    elif test_type == "T2_change_num_of_clusters":
+
+        # Test Description:
+        # In this test, we will be evaluating our framework's performance when you change the number of clusters used against the 
+        # central server gossip network architecture. There are 50 clients total, but for our architecture, we use the following 
+        # number of clusters: 5 clusters (10 clients per cluster), 10 clusters (5 clients per cluster), and 25 clusters (2 clients
+        # per cluster). We evaluate the accuracy, loss, and communication round time, and plot the results.
+
+        n_clients = 50
+    
+        # get all of the client data and the testloader
+        client_data, testloader = build_mnist(n_clients, args.iid_alpha, args.batch_size, seed=args.seed)
+
+        # ------------------------
+        # creating the parameter server model for the cluster arch with 5 clusters (10 clients per cluster)
+        cluster_arch_server_model_5 = ConvNet()
+        # our framework
+        accuracies_cluster_comm_5, losses_cluster_comm_5, times_cluster_comm_5 = fl_semidecentralized_cluster_train(cluster_arch_server_model_5, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader, "T2_change_num_of_clusters", num_clusters=5)
+        # ------------------------
+
+        # ------------------------
+        # creating the parameter server model for the cluster arch with 10 clusters (5 clients per cluster)
+        cluster_arch_server_model_10 = ConvNet()
+        # our framework
+        accuracies_cluster_comm_10, losses_cluster_comm_10, times_cluster_comm_10 = fl_semidecentralized_cluster_train(cluster_arch_server_model_10, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader, "T2_change_num_of_clusters", num_clusters=10)
+        # ------------------------
+
+        # ------------------------
+        # creating the parameter server model for the cluster arch with 25 clusters (2 clients per cluster)
+        cluster_arch_server_model_25 = ConvNet()
+        # our framework
+        accuracies_cluster_comm_25, losses_cluster_comm_25, times_cluster_comm_25 = fl_semidecentralized_cluster_train(cluster_arch_server_model_25, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader, "T2_change_num_of_clusters", num_clusters=25)
+        # ------------------------
+
+        # ------------------------
+        # creating the parameter server model for the central arch
+        central_arch_server_model = ConvNet()
+        # centralized fully sync SGD
+        accuracies_central_comm, losses_central_comm, times_central_comm = fl_centralized_train(central_arch_server_model, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader)
+        # ------------------------
+
+        plot_labels = ["Our Semi-decentralized Framework (clusters = 5)", "Our Semi-decentralized Framework (clusters = 10)",  "Our Semi-decentralized Framework (clusters = 25)", "Fully Sync SGD (centralized)"]
+        accuracies = [accuracies_cluster_comm_5, accuracies_cluster_comm_10, accuracies_cluster_comm_25, accuracies_central_comm]
+        losses = [losses_cluster_comm_5, losses_cluster_comm_10, losses_cluster_comm_25, losses_central_comm]
+        times = [times_cluster_comm_5, times_cluster_comm_10, times_cluster_comm_25, times_central_comm]
+        
+        # plot everything
+        plot_acc(accuracies, test_type, plot_labels, n_clients, 'Accuracy w/ adapting num of clusters')
+        plot_loss(losses, test_type, plot_labels, n_clients, 'Loss w/ adapting num of clusters')
+        plot_comm_times(times, test_type, plot_labels, n_clients, 'Communication Times w/ adapting num of clusters')
+
+    elif test_type == "T3_scale_up_num_of_clients":
+
+        # Test Description:
+        # In this test, we will be evaluating our framework's performance when we scale up the number of clients against the baseline
+        # central server gossip network architecture. We scale up the number of clients to 150. We evaluate the accuracy, loss, and 
+        # communication round time, and plot the results.
+
+        n_clients = 150
+    
+        # get all of the client data and the testloader
+        client_data, testloader = build_mnist(n_clients, args.iid_alpha, args.batch_size, seed=args.seed)
+
+        # ------------------------
+        # creating the parameter server model for the cluster arch
+        cluster_arch_server_model = ConvNet()
+        # our framework
+        accuracies_cluster_comm, losses_cluster_comm, times_cluster_comm = fl_semidecentralized_cluster_train(cluster_arch_server_model, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader, "T3_scale_up_num_of_clients", num_clusters=10)
+        # ------------------------
+
+        # ------------------------
+        # creating the parameter server model for the central arch
+        central_arch_server_model = ConvNet()
+        # centralized fully sync SGD
+        accuracies_central_comm, losses_central_comm, times_central_comm = fl_centralized_train(central_arch_server_model, client_data, args.comm_rounds, args.lr, args.momentum, args.local_iters, args.straggler_max_delay, testloader)
+        # ------------------------
+
+        plot_labels = ["Our Semi-decentralized Framework (clusters = 10)", "Fully Sync SGD (centralized)"]
+        accuracies = [accuracies_cluster_comm, accuracies_central_comm]
+        losses = [losses_cluster_comm, losses_central_comm]
+        times = [times_cluster_comm, times_central_comm]
+        
+        # plot everything
+        plot_acc(accuracies, test_type, plot_labels, n_clients, 'Accuracy w/ scaled up num of clients (150)')
+        plot_loss(losses, test_type, plot_labels, n_clients, 'Loss w/ scaled up num of clients (150)')
+        plot_comm_times(times, test_type, plot_labels, n_clients, 'Communication Times w/ scaled up num of clients (150)')
+        
+    else:
+        print("Unknown test type")
